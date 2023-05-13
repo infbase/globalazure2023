d23870da6d9f6ff99cd310e419ac1fe90accdb20
Nuno Guedes <nunoguedes@hotmail.com>
2023-05-13 05:16:07 +0100

    updated examples

--- container-registry/examples/scripts/create-task.sh
+++ container-registry/examples/scripts/create-task.sh
@@ -4,7 +4,7 @@ az acr task create \
     --image helloworld:{{.Run.ID}} \
     --context https://github.com/infbase/globalazure2023.git#main \
     --file container-registry/examples/container-image/Dockerfile \
-    --git-access-token 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 \
-    --pull-request-trigger-enabled true \
+    --commit-trigger-enabled false \
     --base-image-trigger-enabled true \
-    --schedule "dailyTimer:0 12 * * Mon-Fri"
+    --schedule "dailyTimer:0 12 * * Mon-Fri" \
+    --verbose
